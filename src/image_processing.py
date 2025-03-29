40c854789b896b6e0e42533481e1cf4d91910e44
nehab2007 <nehabhandari1107@gmail.com>
2025-03-29 15:47:16 +0530

    Update image_processing.py

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,41 +1,43 @@
 import tensorflow as tf
 import numpy as np
 import PIL.Image
-import matplotlib.pyplot as plt
 
-def load_img(path_to_img):
-    """Loads and preprocesses an image from a given path."""
-    max_dim = 512
-    img = tf.io.read_file(path_to_img)
+def load_img(img_path, max_dim=512):
+    """
+    Loads and preprocesses an image from a given path.
+    - Rescales the image while maintaining aspect ratio.
+    - Converts it to a float32 tensor.
+
+    Args:
+        img_path (str): Path to the image.
+        max_dim (int): Maximum dimension to resize to.
+
+    Returns:
+        tf.Tensor: Preprocessed image tensor.
+    """
+    img = tf.io.read_file(img_path)
     img = tf.image.decode_image(img, channels=3)
     img = tf.image.convert_image_dtype(img, tf.float32)
 
+    # Resize while maintaining aspect ratio
     shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-    long_dim = max(shape)
-    scale = max_dim / long_dim
-
+    scale = max_dim / tf.reduce_max(shape)
     new_shape = tf.cast(shape * scale, tf.int32)
-    img = tf.image.resize(img, new_shape)
-    img = img[tf.newaxis, :]
-    return img
 
-def imshow(image, title=None):
-    """Displays an image using Matplotlib."""
-    if len(image.shape) > 3:
-        image = tf.squeeze(image, axis=0)
-    plt.imshow(image)
-    if title:
-        plt.title(title)
-    plt.show()
+    img = tf.image.resize(img, new_shape)
+    return tf.expand_dims(img, axis=0)
 
 def tensor_to_image(tensor):
-    """Converts a TensorFlow tensor to a PIL Image."""
-    tensor = tensor * 255
-    tensor = np.array(tensor, dtype=np.uint8)
-    if np.ndim(tensor) > 3:
-        assert tensor.shape[0] == 1
-        tensor = tensor[0]
+    """
+    Converts a TensorFlow tensor to a PIL Image.
+
+    Args:
+        tensor (tf.Tensor): Tensor representing an image.
+
+    Returns:
+        PIL.Image: Converted image.
+    """
+    tensor = np.array(tensor * 255, dtype=np.uint8)
+    if tensor.ndim > 3:
+        tensor = tensor[0]  # Remove batch dimension
     return PIL.Image.fromarray(tensor)
-
-
-path_to_img = 'F:\Art style transfer project\data\input_images\inp_img1.jpeg'
